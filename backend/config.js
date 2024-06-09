25e9fc52d2e520560fe96f6b9b42280a1793feb5
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-09 09:51:16 -0700

    Removed a file

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = 5555;
-
-<<<<<<< HEAD
-export const  mongoDBURL = 'mongodb+srv://EmmanuelMelvin:chidi1234@cluster0.x0szrgk.mongodb.net/bookscollection?retryWrites=true&w=majority&appName=Cluster0'
-=======
-export const  mongoDBURL = 'mongodb+srv://EmmanuelMelvin:Emmanuelchidi@2005@cluster0.x0szrgk.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
->>>>>>> fcb16325d6804e56026b471dc2bf8d0189950df3
