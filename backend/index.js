3a2bfea71fa7c31c7096e129b3d3214cf7fb4272
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-10 14:53:45 -0700

    Adding frontend packages

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import { PORT , mongoDBURL } from './config.js';
-import mongoose from 'mongoose'
-
-import { bookRoute } from './routes/bookRoutes.js'
+import mongoose from 'mongoose';
+import cors from 'cors'
+import bookRoute from './routes/bookRoutes.js'
 
 const app = express();
 //middleware for passing JSON 
 app.use(express.json())
 
 app.use('/books' , bookRoute);
+//Methods of using CORS
+//Oprion1: Allow all origins with defaults of cors()
+app.use(cors())
+
+//Allowing custom origin
+app.use(cors({
+        methods: ['GET' , 'POST' , 'DELETE'],
+        allowedHeaders: ['Content-Type']
+
+}))
 
 
 //Connecting to mongoDB using mongoose
