5dcac7d62fce4af73a8b3d71410bbd36fcb7dbfc
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-12 05:16:53 -0700

    Added routes to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white" >
-      App
-    </div>
+   <Route>
+    <Route path=''  element={}></Route>
+    <Route path=''  element={}></Route>
+    <Route path=''  element={}></Route>
+    <Route path=''  element={}></Route>
+    <Route path=''  element={}></Route>
+   </Route>
   )
 }
 
