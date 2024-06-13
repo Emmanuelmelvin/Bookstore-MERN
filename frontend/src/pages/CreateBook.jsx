21793d526ce0fd2726949bbead0abf6ccb41f52c
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-13 08:42:46 -0700

    Change #1

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 
 const CreateBook = () => {
   return (
-    <div>
-      <h1>Test Component</h1>
-      <p>This is a simple test component.</p>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+    <div className='bg-red-400 text-black'>
+    Create
     </div>
   )
 }
---
+++ frontend/src/pages/NoMatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NoMatch = () => {
+  return (
+    <div>
+      Page Not Found
+    </div>
+  )
+}
+
+
