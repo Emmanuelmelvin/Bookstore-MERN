9c4eefadc503800fdc07df4f8f8b9bfb500a6b18
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-16 06:37:43 -0700

    Edit detials page

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React , { useEffect , useState} from 'react'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+
 
 const ShowBook = () => {
+    const [book , setBooks] = useState([])
+    const [loading , setLoading] = useState(true)
+    const { id } = useParams()
+
+    useEffect(() => {
+      axios
+      .get(`http://localhost:5000/books/${id}`)
+      .then((response) => {
+        setBooks(response.data)
+        setLoading(false)
+      })
+      .catch(error => {
+        console.log(error)
+        setLoading(false)
+      })
+    } , [])
+   
   return (
-    <div>
-      
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Show Books</h1>
+      {
+        loading? (
+          <Spinner/>
+        ) : (
+          <div className='flex flex-col bg-grey-400 border-2 border-sky-400 rounded-xl w-fit p-4'>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Id</span>
+                <span>{book._id}</span>
+            </div>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Title</span>
+                <span>{book.title}</span>
+            </div>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Author</span>
+                <span>{book.author}</span>
+            </div>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
+                <span>{book.publishYear}</span>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
