e225439b91f9a0702a5386c54c002fcddd8bf3f2
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-19 08:07:38 -0700

    Updates

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner'
+import BackButton from '../components/BackButton'
+import { useState } from 'react'
 
 const DeleteBook = () => {
+
+  cosnt[loading, setLoading] = useState(true)
+  const { title , setTitle} = useState('')
+  const { author , setAuthor} = useState('')
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  const handelDeleteBook = (event) => {
+    event.preventDefault()
+    axios
+    .delete(`https://localhost:5000/books/${id}`)
+    .then (() => {
+      setLoading(false)
+      navigate('/')
+    })
+    .catch((error) => {
+      setLoading(false)
+      alert('An error occured')
+      console.log(error)
+    })
+  }
+   useEffect(() => {
+     axios
+     .get(`https://localhost:5000/books${id}`)
+     .then(response => {
+       setTitle(response.data.title)
+       setAuthor(response.data.author)
+       setLoading(false)
+    })
+   })
+
+
   return (
-    <div>
-      
+    <div className='p-4'>
+        <BackButton/>
+        <h1>Delete Book</h1>
+        {
+          loadimg ? <Spinner/> : (
+            <div> 
+
+            </div>
+          )
+        }
     </div>
   )
 }
